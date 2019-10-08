5041e84024a681b2685a4fe1bc66819a13ca1bd4
Json Bourne <jsonbourneagain@outlook.com>
2019-10-08 21:01:12 +0530

    Refactored the FirstNonRepeatedCharacter() method

--- Program.cs
+++ Program.cs
@@ -39,7 +39,7 @@ namespace DataStructuresPart1
         {
             var ar = input.Replace(" ", "");
             Dictionary<char, int> charDic = new Dictionary<char, int>();
-
+            char retChar = ' ';
             foreach (char c in ar)
             {
                 if (!charDic.ContainsKey(c))
@@ -47,26 +47,16 @@ namespace DataStructuresPart1
                 else
                     charDic[c]++;
             }
-
-            int i = default;
-            foreach (var c in charDic.Values)
+            foreach (var c in charDic.Keys)
             {
-                if (c == 1)
+                if (charDic[c] == 1)
+                {
+                    retChar = c;
                     break;
-                i++;
-            }
-            int j = default;
-            char cr = ' ';
+                }
 
-            foreach (var d in charDic.Keys)
-            {
-                cr = d;
-                if (j == i)
-                    break;
-                j++;
             }
-
-            return cr;
+            return retChar;
 
         }
     }
